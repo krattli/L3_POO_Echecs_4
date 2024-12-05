cf37fa4c7989ac96fa70babe18ecbc80b966b274
Raphael Krattli <raphael.krattli@etu-paris1.fr>
2024-12-05 13:20:26 +0100

    transformation de l'échiquier en static

--- src/main/java/fr/pantheonsorbonne/miage/game/Echiquier.java
+++ src/main/java/fr/pantheonsorbonne/miage/game/Echiquier.java
@@ -1,47 +1,95 @@
 package fr.pantheonsorbonne.miage.game;
 
 import fr.pantheonsorbonne.miage.enums.Color;
+import fr.pantheonsorbonne.miage.exception.WrongCaseFormatException;
+import fr.pantheonsorbonne.miage.game.Pieces.pieces_simple.Pion;
 import fr.pantheonsorbonne.miage.playerRelatedStuff.Player;
+import fr.pantheonsorbonne.miage.playerRelatedStuff.PlayerBot;
 
 public class Echiquier {
-    private Piece[][] plateau;
-    private Player[] players;
-
     private static final int TAILLE = 14;
+    private static Piece[][] plateau = new Piece[TAILLE][TAILLE];
+    private static Player[] players;
 
-    public Echiquier(Player joueur1, Player joueur2, Player joueur3, Player joueur4) {
+    private Echiquier() {}
+
+    public static void addPlayers(Player joueur1, Player joueur2, Player joueur3, Player joueur4) throws WrongCaseFormatException {
         joueur1.setColor(Color.RED);
         joueur2.setColor(Color.GREEN);
         joueur3.setColor(Color.YELLOW);
         joueur4.setColor(Color.BLUE);
-        Player[] players = new Player[]{joueur1, joueur2, joueur3, joueur4};
-        this.plateau = new Piece[TAILLE][TAILLE];
-        this.players = players;
-        EchiquierInitializer.initialiser(this);
-    }
-    
-    public Player[] getPlayers() {
-        return this.players;
-    }
-    public Piece[][] getPlateau() {
-        return this.plateau;
+        Echiquier.players = new Player[]{joueur1, joueur2, joueur3, joueur4};
     }
 
-    public void jouerCoup(Coup coup) {
+    public static void initAllPieces () throws WrongCaseFormatException {
+        if (players.length == 4) {
+            plateau = new Piece[TAILLE][TAILLE];
+            EchiquierInitializer.initialiser();
+        }
+    }
+    
+    public static Player[] getPlayers() {
+        return players;
+    }
+    public static Piece[][] getPlateau() {
+        return plateau;
+    }
+
+    public static void setPieceToPosition(Piece piece, Case position){
+
+        if(piece.getPosition() != null) {
+            int[] coordDepart = piece.getPosition().getCoordInt();
+            plateau[coordDepart[0]][coordDepart[1]] = null;
+        }
+
+        if(position == null){
+            piece.setPosition(null);
+        }
+        else {
+            int[] coordDestination = position.getCoordInt();
+            plateau[coordDestination[0]][coordDestination[1]] = piece;
+            piece.setPosition(position);
+        }
+    }
+
+    //Peut etre que cette méthode est completement innutile, on verra
+    public static Piece[][] rotateCopyToRightBy90(int nombreRotation){
+        Piece[][] rotatedPlateau = plateau;
+        for (int rotation=0; rotation<nombreRotation; rotation++){
+            for (int i=0; i<TAILLE; i++){
+                for (int j=0; j<TAILLE; j++){
+                    if(i!=j){
+                        Piece tempVar = rotatedPlateau[i][j];
+                        rotatedPlateau[i][j] = rotatedPlateau[j][i];
+                        rotatedPlateau[j][i] = tempVar;
+                    }
+                }
+            }
+            for (int i=0; i<TAILLE; i++){
+                for (int j=0; j<TAILLE; j++){
+                    if(i!=j){
+                        Piece tempVar = rotatedPlateau[i][j];
+                        plateau[i][j] = plateau[i][TAILLE - i - 1];
+                        plateau[i][TAILLE - j - 1] = tempVar;
+                    }
+                }
+            }
+        }
+        return rotatedPlateau;
+    }
+
+    public static void jouerCoup(Coup coup) {
         // Implémenter la logique pour jouer un coup
     }
 
-    public void movePiece(Piece piece, Case position) {
-        plateau[position.getColonne().ordinal()][position.getLigne()-1] = piece;
-        piece.setPosition(position);
-        //update la pos de la pièce prise si la case est dékja occupée
+    public static void emptyCell(Case position) {
+        //verifier si les lignes sont bien les bonnes
+        plateau[position.getColonne().ordinal()][position.getLigne().ordinal()] = null;
     }
 
-    public void emptyCell(Case position) {
-        plateau[position.getColonne().ordinal()][position.getLigne()-1] = null;
-    }
-
-    public Piece getPieceAt(Case position) {
-        return this.plateau[position.getColonne().ordinal()][position.getLigne()-1];
+    public static Piece getPieceAt(Case position) {
+        //verifier si les lignes sont bien les bonnes
+        int[] coord = position.getCoordInt();
+        return plateau[coord[0]][coord[1]];
     }
 }
