f3271266eb64996225be87658b9015580baf658e
Raphael Krattli <raphael.krattli@etu-paris1.fr>
2024-12-10 14:57:39 +0100

    test cases, sep en 2 fichiers

---
+++ src/test/java/fr/pantheonsorbonne/miage/game/CasesPlateauTest.java
@@ -0,0 +1,60 @@
+package fr.pantheonsorbonne.miage.game;
+
+import fr.pantheonsorbonne.miage.enums.Colonne;
+import fr.pantheonsorbonne.miage.enums.Ligne;
+import fr.pantheonsorbonne.miage.exception.WrongCaseFormatException;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CasesPlateauTest {
+    @Test
+    void areValidCases() throws WrongCaseFormatException {
+
+        //Dans les coins (faux)
+        Case c1 = new Case("C3");
+        Case c2 = new Case("L12");
+        Case c3 = new Case("C12");
+        Case c4 = new Case("L3");
+
+        //Adjacent aux coins (vrai)
+        Case c5 = new Case("C4");
+        Case c6 = new Case("K4");
+        Case c7 = new Case("L11");
+        Case c8 = new Case("C4");
+
+        // En plein milieu du plateau (vrai)
+        Case c9 = new Case("C4");
+        Case c10 = new Case("C4");
+        Case c11 = new Case("C4");
+        Case c12 = new Case("C4");
+
+        Case[] mauvaisesCases = {c1, c2, c3, c4};
+        Case[] bonnesCases = {c5, c6, c7, c8, c9, c10, c11, c12};
+
+        boolean testCases = true;
+        for (Case c : mauvaisesCases) {
+            if (c.isValid()) {
+                testCases = false;
+            }
+        }
+        for (Case c : bonnesCases) {
+            if (!c.isValid()) {
+                testCases = false;
+            }
+        }
+        assertTrue(testCases);
+    }
+
+    @Test
+    void validTranslation() throws WrongCaseFormatException {
+        Case c = new Case("D1");
+
+        Case c1 = c.getValidTranslatedCase(0, 0);
+        Case c2 = c.getValidTranslatedCase(8, 0);
+        Case c3 = c.getValidTranslatedCase(0, 0);
+        Case c4 = c.getValidTranslatedCase(0, 0);
+
+        assertNotNull(c2);
+    }
+}
--- src/test/java/fr/pantheonsorbonne/miage/game/ConstructeurCaseTest.java
+++ src/test/java/fr/pantheonsorbonne/miage/game/ConstructeurCaseTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class CasesPlateauTest {
+class ConstructeurCaseTest {
 
     @Test
     void Constructeur1CoordAlgebriques() {
@@ -59,55 +59,5 @@ class CasesPlateauTest {
         Case c1 = new Case("N14");
         int[] expectedCoordinates = {0, 13};
         assertArrayEquals(expectedCoordinates, c1.getCoordInt());
-    }
-
-    @Test
-    void areValidCases() throws WrongCaseFormatException {
-
-        //Dans les coins (faux)
-        Case c1 = new Case("C3");
-        Case c2 = new Case("L12");
-        Case c3 = new Case("C12");
-        Case c4 = new Case("L3");
-
-        //Adjacent aux coins (vrai)
-        Case c5 = new Case("C4");
-        Case c6 = new Case("K4");
-        Case c7 = new Case("L11");
-        Case c8 = new Case("C4");
-
-        // En plein milieu du plateau (vrai)
-        Case c9 = new Case("C4");
-        Case c10 = new Case("C4");
-        Case c11 = new Case("C4");
-        Case c12 = new Case("C4");
-
-        Case[] mauvaisesCases = {c1, c2, c3, c4};
-        Case[] bonnesCases = {c5, c6, c7, c8, c9, c10, c11, c12};
-
-        boolean testCases = true;
-        for (Case c : mauvaisesCases) {
-            if (c.isValid()) {
-                testCases = false;
-            }
-        }
-        for (Case c : bonnesCases) {
-            if (!c.isValid()) {
-                testCases = false;
-            }
-        }
-        assertTrue(testCases);
-    }
-
-    @Test
-    void validTranslation() throws WrongCaseFormatException {
-        Case c = new Case("D1");
-
-        Case c1 = c.getValidTranslatedCase(0, 0);
-        Case c2 = c.getValidTranslatedCase(8, 0);
-        Case c3 = c.getValidTranslatedCase(0, 0);
-        Case c4 = c.getValidTranslatedCase(0, 0);
-
-        assertNotNull(c2);
     }
 }
