f54a10be951bf70f9af24e585aec41587e1be660
Raphael Krattli <raphael.krattli@etu-paris1.fr>
2024-12-10 13:31:13 +0100

    simplification

--- src/main/java/fr/pantheonsorbonne/miage/game/EchiquierInitializer.java
+++ src/main/java/fr/pantheonsorbonne/miage/game/EchiquierInitializer.java
@@ -26,29 +26,31 @@ public class EchiquierInitializer {
 
     private static void initialiserPlayer(Player player) throws WrongCaseFormatException {
 
-        Case posRoiInit = new Case("H1").getCorespondingCoordRotatedBy90(player.getColor().ordinal());
+        int numberOfRotation = player.getColor().ordinal();
+
+        Case posRoiInit = new Case("H1").getCorespondingCoordRotatedBy90(numberOfRotation);
         Roi roi = new Roi(player, posRoiInit);
 
-        Case posDameInit = new Case("G1").getCorespondingCoordRotatedBy90(player.getColor().ordinal());
+        Case posDameInit = new Case("G1").getCorespondingCoordRotatedBy90(numberOfRotation);
         Dame dame = new Dame(player, posDameInit);
 
-        Case posFou1Init = new Case("F1").getCorespondingCoordRotatedBy90(player.getColor().ordinal());
-        Case posFou2Init = new Case("I1").getCorespondingCoordRotatedBy90(player.getColor().ordinal());
+        Case posFou1Init = new Case("F1").getCorespondingCoordRotatedBy90(numberOfRotation);
+        Case posFou2Init = new Case("I1").getCorespondingCoordRotatedBy90(numberOfRotation);
         Fou fou1 = new Fou(player, posFou1Init);
         Fou fou2 = new Fou(player, posFou2Init);
 
-        Case posCavalier1Init = new Case("E1").getCorespondingCoordRotatedBy90(player.getColor().ordinal());
-        Case posCavalier2Init = new Case("J1").getCorespondingCoordRotatedBy90(player.getColor().ordinal());
+        Case posCavalier1Init = new Case("E1").getCorespondingCoordRotatedBy90(numberOfRotation);
+        Case posCavalier2Init = new Case("J1").getCorespondingCoordRotatedBy90(numberOfRotation);
         Cavalier cavalier1 = new Cavalier(player, posCavalier1Init);
         Cavalier cavalier2 = new Cavalier(player, posCavalier2Init);
 
-        Case posTour1Init = new Case("D1").getCorespondingCoordRotatedBy90(player.getColor().ordinal());
-        Case posTour2Init = new Case("K1").getCorespondingCoordRotatedBy90(player.getColor().ordinal());
+        Case posTour1Init = new Case("D1").getCorespondingCoordRotatedBy90(numberOfRotation);
+        Case posTour2Init = new Case("K1").getCorespondingCoordRotatedBy90(numberOfRotation);
         Tour tour1 = new Tour(player, posTour1Init);
         Tour tour2 = new Tour(player, posTour2Init);
 
         for (int i = 3 ; i <= 10 ; i++){
-            Case posPionInit = new Case(Colonne.values()[i].toString()+"2").getCorespondingCoordRotatedBy90(player.getColor().ordinal());
+            Case posPionInit = new Case(Colonne.values()[i].toString()+"2").getCorespondingCoordRotatedBy90(numberOfRotation);
             Pion pion = new Pion(player, posPionInit);
         }
     }
