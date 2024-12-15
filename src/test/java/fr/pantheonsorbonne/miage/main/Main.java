616a2b53ec50c28e46906ad40e4a06dcf296373f
Raphael Krattli <raphael.krattli@etu-paris1.fr>
2024-12-15 20:41:43 +0100

    osef

--- src/test/java/fr/pantheonsorbonne/miage/main/Main.java
+++ src/test/java/fr/pantheonsorbonne/miage/main/Main.java
@@ -1,14 +1,16 @@
 package fr.pantheonsorbonne.miage.main;
 
-import fr.pantheonsorbonne.miage.exception.WrongCaseFormatException;
+import fr.pantheonsorbonne.miage.game.Coup;
 import fr.pantheonsorbonne.miage.game.Echiquier;
 import fr.pantheonsorbonne.miage.game.pieces.simple.Dame;
-import fr.pantheonsorbonne.miage.game.pieces.simple.Roi;
+import fr.pantheonsorbonne.miage.game.pieces.simple.Pion;
 import fr.pantheonsorbonne.miage.playerRelatedStuff.Player;
 import fr.pantheonsorbonne.miage.playerRelatedStuff.PlayerBot;
 
+import java.util.ArrayList;
+
 public class Main {
-    public static void main(String[] args) throws WrongCaseFormatException {
+    public static void main(String[] args) {
 
         PlayerBot j1 = new PlayerBot("Gary Kasparov");
         PlayerBot j2 = new PlayerBot("Hikaru Nakamura");
@@ -18,8 +20,11 @@ public class Main {
         Echiquier plateau = new Echiquier(new Player[] {j1, j2,j3, j4});
 
         Dame roi = new Dame(j1, "G7");
+        Pion p = new Pion(j2,"G4");
         plateau.computeMenaces();
+        ArrayList<Coup> coups = roi.getAllPossibleMoves();
         plateau.printCasesMenacees(j1);
+        System.out.println(coups.toString());
 
     }
 }
