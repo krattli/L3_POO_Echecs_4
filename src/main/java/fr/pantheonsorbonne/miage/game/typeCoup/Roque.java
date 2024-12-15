7da6114930ab2de75af1ed70d942bb4287f71684
Raphael Krattli <raphael.krattli@etu-paris1.fr>
2024-12-15 19:10:32 +0100

    rename

--- src/main/java/fr/pantheonsorbonne/miage/game/typeCoup/Roque.java
+++ src/main/java/fr/pantheonsorbonne/miage/game/typeCoup/Roque.java
@@ -4,8 +4,8 @@ import fr.pantheonsorbonne.miage.game.Case;
 import fr.pantheonsorbonne.miage.game.Coup;
 import fr.pantheonsorbonne.miage.game.Piece;
 
-public class Rock extends Coup {
-    public Rock(Piece piece, Case arrivee) {
+public class Roque extends Coup {
+    public Roque(Piece piece, Case arrivee) {
         super(piece, arrivee);
     }
 
