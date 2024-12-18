ce686b6041e2dc059ffe59c7ebc63eeb68f0b014
Raphael Krattli <raphael.krattli@etu-paris1.fr>
2024-12-18 17:17:49 +0100

    debut super pieces

--- src/main/java/fr/pantheonsorbonne/miage/game/Coup.java
+++ src/main/java/fr/pantheonsorbonne/miage/game/Coup.java
@@ -1,5 +1,7 @@
 package fr.pantheonsorbonne.miage.game;
 
+import fr.pantheonsorbonne.miage.game.typeCoup.SuperDeplacement;
+
 public abstract class Coup {
     private Case depart;
     private Case arrivee;
@@ -15,9 +17,26 @@ public abstract class Coup {
         this(piece, new Case(arrivee));
     }
 
+    public static Coup stringToCoup(String s) {
+        Coup c;
+        if(s.startsWith("S")) {
+            c = handleSuperDeplacement(s);
+        }
+        else {
+            Case Depart = new Case(s.substring(1, 3));
+        }
+        return null;
+    }
+
     public Case getDepart() {return depart;}
     public Case getArrivee() {return arrivee;}
     public Piece getPiece() {return piece;}
 
     public abstract String toString();
+
+    private static SuperDeplacement handleSuperDeplacement(String s) {
+        SuperDeplacement superDeplacement = null;
+        Case Depart = new Case(s.substring(1, 3));
+        return superDeplacement;
+    }
 }
--- src/main/java/fr/pantheonsorbonne/miage/game/typeCoup/SuperDeplacement.java
+++ src/main/java/fr/pantheonsorbonne/miage/game/typeCoup/SuperDeplacement.java
@@ -4,13 +4,27 @@ import fr.pantheonsorbonne.miage.game.Case;
 import fr.pantheonsorbonne.miage.game.Coup;
 import fr.pantheonsorbonne.miage.game.Piece;
 
+import java.util.ArrayList;
+
 public class SuperDeplacement extends Coup {
+    private ArrayList<Piece> piecesMangees;
+
     public SuperDeplacement(Piece piece, Case arrivee) {
         super(piece, arrivee);
     }
 
     @Override
     public String toString() {
-        return "";
+        String nombreDePrises;
+        if (this.piecesMangees == null || this.piecesMangees.isEmpty()) {
+            nombreDePrises = "-";
+        } else {
+            nombreDePrises = "x".repeat(this.piecesMangees.size());
+        }
+        return "S"
+                + this.getPiece().getClass().getSimpleName().charAt(5)
+                + this.getDepart().toString()
+                + nombreDePrises
+                + this.getArrivee().toString();
     }
 }
